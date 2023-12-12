5752663ae5ff49760d245cde1a333defc818dd6d
Jodie <jodielee062788@gmail.com>
2023-12-13 08:36:12 +1100

    Update html file

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        number: 1,
+        question: "The function and var are known as:",
+        choices: [
+            "Keywords", 
+            "Data types", 
+            "Declaration Statements", 
+            "Prototypes"],
+        answer: "Declaration Statements"
+    },
+    {
+        number: 2,
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        choices: [
+            "document.write()", 
+            "console.log()", 
+            "window.alert()", 
+            "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        number: 3,
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        choices: [
+            "stringify()", 
+            "parse()", 
+            "convert()", 
+            "None of the above"],
+        answer: "stringify()"
+    },
+    {
+        number: 4,
+        question: "How to stop an interval timer in Javascript?",
+        choices: [
+            "clearTimer", 
+            "clearInterval", 
+            "intervalOver", 
+            "intervalStop"],
+        answer: "clearInterval"
+    },
+    {
+        number: 5,
+        question: "How do we write a comment in javascript?",
+        choices: [
+            "/* */", 
+            "#", 
+            "//", 
+            "$ $"],
+        answer: "//"
+    },
+];
